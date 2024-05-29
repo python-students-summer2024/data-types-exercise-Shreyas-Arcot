99801405e32038ae9c2944039c129a7d139f99bd
Shreyas_Arcot <sa7971@nyu.edu>
2024-05-29 01:54:50 -0400

    Finished Data Types HW

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ def main():
     """
     This function runs the code you have placed in the other file.
     I suggest commenting out any parts of the code below that you do not wish to run at the moment.
-    Focus on one problem at a time.  For example, by default I have commented out all lines except line 9.
+    Focus on one problem at a time. 
+    For example, by default I have commented out all lines except line 9.
     """
     calculate_profit()
-    # calculate_quotient_and_remainder()
-    # calculate_miles_per_gallon()
-    # align_text()
+    calculate_quotient_and_remainder()
+    calculate_miles_per_gallon()
+    align_text()
 
 
 main()
--- solutions.py
+++ solutions.py
@@ -11,7 +11,11 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
-
+    projected_amount_of_sales = input("What is the Projected Amount of Total Sales?")
+    projected_sales_int = int(projected_amount_of_sales)
+    profit_unformatted=int(0.23 * projected_sales_int)
+    profit = f"{profit_unformatted:,.2f}"
+    print("profit: $" + profit)
 
 def calculate_quotient_and_remainder():
     """
@@ -22,7 +26,11 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
-
+    number_one = input("Enter number #1: ")
+    number_two = input("Enter number #2: ")
+    x = int(number_one)
+    y = int(number_two)
+    print(number_two + " goes into " + number_one + " a total of " + str(x // y) + " times with a remainder of " + str(x % y))
 
 def calculate_miles_per_gallon():
     """
@@ -35,6 +43,12 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
+    miles_driven = input("How many miles did you drive?")
+    gallons_gas = input("How many gallons of gas did you use?")
+    m = int(miles_driven)
+    g = int(gallons_gas)
+    miles_per_gallon = m / g
+    print("Miles driven: " + str(m) + "\nGas used (gallons): " + str(g) + "\nMiles per gallon: " + str(miles_per_gallon))
 
 
 def align_text():
@@ -53,3 +67,22 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+    price_number_one = input("Enter price #1: ")
+    price_number_two = input("Enter price #2: ")
+    price_number_three = input("Enter price #3: ")
+    price_one_float = float(price_number_one)
+    price_two_float = float(price_number_two)
+    price_three_float = float(price_number_three)
+
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+    price_one = format(price_one_float, '0.2f')
+    price_two = format(price_two_float, '0.2f')
+    price_three = format(price_three_float, '0.2f')
+    print("\nHere are your prices!\n")
+    print(
+        f"{'Price #1: $'}{price_one:>8}",
+        f"{'Price #2: $'}{price_two:>8}",
+        f"{'Price #3: $'}{price_three:>8}",
+        sep="\n"
+    )
